3ada9cbebfa5ce59ebea2ef42cf01dfc44e225af
Patrick Gomes <patrickgomes@hotmail.com>
2016-02-19 14:19:50 -0600

    REAL first commit

---
+++ app/models/recipe.rb
@@ -0,0 +1,15 @@
+class Recipe
+	include HTTParty
+
+
+	default_options.update(verify: false) # Turn off SSL verification
+  	key_value = ENV['FOOD2FORK_KEY'] || "c36df78b48febf4a208c14f7121efe7c"
+	hostport = ENV['FOOD2FORK_SERVER_AND_PORT'] || 'food2fork.com'
+  	base_uri "http://#{hostport}/api"
+  	default_params key: ENV["FOOD2FORK_KEY"]
+  	format :json
+
+  	def self.for (keyword)
+   		get("/search", query: {q: keyword})["recipes"]
+  	end
+end
